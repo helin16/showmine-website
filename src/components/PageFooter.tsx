49f3c90ea984f489ccb62b3c86dc9ac254b82c31
Lin He <showmine.sysbox@gmail.com>
2024-04-23 13:14:41 +1000

    master: disabling all OLD structure.

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -24,18 +24,58 @@ export default function PageFooter() {
         <div className="xl:w-1/4">
           <div className="text-2xl mt-2 mb-6">Contact Info</div>
           <div className="flex pb-3 items-start">
-            <div className="w-1/12"></div>
+            <div className="p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+                fill="currentColor"
+                className="w-5 h-5 bi bi-list"
+              >
+                <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+              </svg>
+            </div>
             <div>
               Unit 108/45 Gilby Rd <br />
               Mount Waverley VIC 3149
             </div>
           </div>
-          <div className="flex pb-3">
-            <div className="w-1/12"></div>
+          <div className="flex pb-3 items-center">
+            <div className="p-2">
+              <svg
+                className="w-5 h-5 bi bi-list"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z"
+                  clip-rule="evenodd"
+                />
+                <path
+                  fill="currentColor"
+                  d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z"
+                />
+              </svg>
+            </div>
             <div>WeChat Official: showmine6</div>
           </div>
-          <div className="flex pb-3">
-            <div className="w-1/12"></div>
+          <div className="flex pb-3 items-center">
+            <div className="p-2">
+              <svg
+                className="bi bi-list h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12 5a7 7 0 0 0-7 7v1.17c.313-.11.65-.17 1-.17h2a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1H6a3 3 0 0 1-3-3v-6a9 9 0 0 1 18 0v6a3 3 0 0 1-3 3h-2a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h2c.35 0 .687.06 1 .17V12a7 7 0 0 0-7-7Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
             <div>Customer Service: showmine688</div>
           </div>
         </div>
